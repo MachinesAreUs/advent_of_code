60297ceb5ba6a8edc2564c535e293f4e0d9f5780
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-12-23 15:37:50 -0600

    Pimped Machine.ex

--- lib/day_23_opening_the_turing_lock/machine.ex
+++ lib/day_23_opening_the_turing_lock/machine.ex
@@ -1,57 +1,47 @@
 defmodule Machine do
 
   @do_nothing [{:jump, 0}]
-  defstruct reg_a: 0, reg_b: 0, addr: 0, program: @do_nothing
+  defstruct rx_a: 0, rx_b: 0, addr: 0, program: @do_nothing
 
   def run(m=%Machine{addr: addr, program: program}) do
-    if addr < 0 or addr >= length(program) do
-      m.reg_b
-    else
-      run(step(m))
+    case addr < 0 or addr >= length(program) do
+      true -> m.rx_b
+      _    -> run(step(m))
     end
   end
 
   def step(m = %Machine{}), do: step m, Enum.at(m.program, m.addr)
 
-  def step(m = %Machine{}, {:hlf, :a}), do: %{m | reg_a: div(m.reg_a, 2)} |> inc_addr
-  def step(m = %Machine{}, {:hlf, :b}), do: %{m | reg_b: div(m.reg_b, 2)} |> inc_addr
+  def step(m = %Machine{}, {:hlf, :a}), do: %{m | rx_a: div(m.rx_a, 2)} |> inc_addr
+  def step(m = %Machine{}, {:hlf, :b}), do: %{m | rx_b: div(m.rx_b, 2)} |> inc_addr
 
-  def step(m = %Machine{}, {:tpl, :a}), do: %{m | reg_a: m.reg_a * 3} |> inc_addr
-  def step(m = %Machine{}, {:tpl, :b}), do: %{m | reg_b: m.reg_b * 3} |> inc_addr
+  def step(m = %Machine{}, {:tpl, :a}), do: %{m | rx_a: m.rx_a * 3} |> inc_addr
+  def step(m = %Machine{}, {:tpl, :b}), do: %{m | rx_b: m.rx_b * 3} |> inc_addr
 
-  def step(m = %Machine{}, {:inc, :a}), do: %{m | reg_a: m.reg_a + 1} |> inc_addr
-  def step(m = %Machine{}, {:inc, :b}), do: %{m | reg_b: m.reg_b + 1} |> inc_addr
+  def step(m = %Machine{}, {:inc, :a}), do: %{m | rx_a: m.rx_a + 1} |> inc_addr
+  def step(m = %Machine{}, {:inc, :b}), do: %{m | rx_b: m.rx_b + 1} |> inc_addr
 
-  def step(m = %Machine{}, {:jmp, x}) when is_integer(x), do: %{m | addr: m.addr + x}
+  def step(m = %Machine{}, {:jmp, ofst}) when is_integer(ofst), do: %{m | addr: m.addr + ofst}
 
-  def step(m = %Machine{}, {:jie, :a, offset}) when is_integer(offset) do
-    case rem(m.reg_a, 2) do
-       0 -> step(m, {:jmp, offset})
+  def step(m = %Machine{}, {:jie, rx, ofst}) when is_atom(rx) and is_integer(ofst) do
+    case rem(rx_value(m, rx), 2) do
+       0 -> step(m, {:jmp, ofst})
        _ -> m |> inc_addr
     end
   end
 
-  def step(m = %Machine{}, {:jie, :b, offset}) when is_integer(offset) do
-    case rem(m.reg_b, 2) do
-       0 -> step(m, {:jmp, offset})
-       _ -> m |> inc_addr
-    end
-  end
-
-  def step(m = %Machine{}, {:jio, :a, offset}) when is_integer(offset) do
-    case m.reg_a do
-       1 -> step(m, {:jmp, offset})
+  def step(m = %Machine{}, {:jio, rx, ofst}) when is_atom(rx) and is_integer(ofst) do
+    case rx_value(m, rx) do
+       1 -> step(m, {:jmp, ofst})
        _ -> m |> inc_addr
     end 
   end
 
-  def step(m = %Machine{}, {:jio, :b, offset}) when is_integer(offset) do
-    case m.reg_b do
-       1 -> step(m, {:jmp, offset})
-       _ -> m |> inc_addr
-    end 
-  end
+  defp inc_addr(m = %Machine{}), do: %{m | addr: m.addr + 1}
 
-  def inc_addr(m = %Machine{}), do: %{m | addr: m.addr + 1}
+  defp rx_value(m, atom) do
+    real_prop = "rx_" <> Atom.to_string(atom) |> String.to_atom  
+    Map.get m, real_prop
+  end
 
 end
