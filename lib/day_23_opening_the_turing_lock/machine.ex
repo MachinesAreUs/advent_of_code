e751eefff8dfdef0090531ba8f34d13e1e9faeae
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-12-24 08:43:36 -0600

    Pattern matching all the way down the rabbit hole

--- lib/day_23_opening_the_turing_lock/machine.ex
+++ lib/day_23_opening_the_turing_lock/machine.ex
@@ -12,24 +12,21 @@ defmodule Machine do
 
   def step(m = %Machine{}), do: step m, Enum.at(m.program, m.addr)
 
-  def step(m = %Machine{}, {:hlf, rx})  when is_atom(rx),     do: rx_set(m, rx, div(rx_val(m, rx), 2)) |> inc_addr
-  def step(m = %Machine{}, {:tpl, rx})  when is_atom(rx),     do: rx_set(m, rx, rx_val(m, rx) * 3)     |> inc_addr
-  def step(m = %Machine{}, {:inc, rx})  when is_atom(rx),     do: rx_set(m, rx, rx_val(m, rx) + 1)     |> inc_addr
-  def step(m = %Machine{}, {:jmp, ofs}) when is_integer(ofs), do: %{m | addr: m.addr + ofs}
+  def step(m = %Machine{}, {:hlf, rx})   when is_atom(rx),      do: rx_set(m, rx, div(rx_val(m, rx), 2)) |> inc_addr
+  def step(m = %Machine{}, {:tpl, rx})   when is_atom(rx),      do: rx_set(m, rx, rx_val(m, rx) * 3)     |> inc_addr
+  def step(m = %Machine{}, {:inc, rx})   when is_atom(rx),      do: rx_set(m, rx, rx_val(m, rx) + 1)     |> inc_addr
+  def step(m = %Machine{}, {:jmp, ofst}) when is_integer(ofst), do: %{m | addr: m.addr + ofst}
 
-  def step(m = %Machine{}, {:jie, rx, ofst}) when is_atom(rx) and is_integer(ofst) do
-    case rem(rx_val(m, rx), 2) do
-       0 -> m |> step {:jmp, ofst}
-       _ -> m |> inc_addr
-    end
-  end
+  def step(m = %Machine{}, {:jie, rx, ofst}) when is_atom(rx) and is_integer(ofst), 
+    do: jump_if(is_even?(rx_val(m, rx)), m, ofst)
 
-  def step(m = %Machine{}, {:jio, rx, ofst}) when is_atom(rx) and is_integer(ofst) do
-    case rx_val(m, rx) do
-       1 -> m |> step {:jmp, ofst}
-       _ -> m |> inc_addr
-    end 
-  end
+  def step(m = %Machine{}, {:jio, rx, ofst}) when is_atom(rx) and is_integer(ofst), 
+    do: jump_if(rx_val(m, rx) == 1, m, ofst)
+
+  defp jump_if(true, m, ofst), do: m |> step {:jmp, ofst}
+  defp jump_if(false, m, _),   do: m |> inc_addr
+
+  defp is_even?(x), do: rem(x, 2) == 0
 
   defp inc_addr(m = %Machine{}), do: %{m | addr: m.addr + 1}
 
